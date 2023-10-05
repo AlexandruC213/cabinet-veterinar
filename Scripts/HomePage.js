ed575146fd7b1a6cebd9e291037f5efba1e83fdb
Chis Alexandru <alexandru_chis213@yahoo.com>
2023-10-05 22:35:58 +0300

    fix js redirect

--- Scripts/HomePage.js
+++ Scripts/HomePage.js
@@ -5,13 +5,13 @@ readMoreAboutMe();
 function visit() {
     const visitButton = document.querySelector('.petCare-container .petCare-information div button');
     visitButton.addEventListener('click', () => {
-      window.location.href = "Servicii.html";
+      window.location.href = "Pages/Servicii.html";
     })
   }
 
 function readMoreAboutMe() {
     const aboutMeButton = document.querySelector('.aboutMe-container .content-aboutMe button');
     aboutMeButton.addEventListener('click', () => {
-        window.location.href = "DespreMine.html";
+        window.location.href = "Pages/DespreMine.html";
     })
 }
